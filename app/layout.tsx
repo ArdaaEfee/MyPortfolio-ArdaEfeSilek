6de1cc6451c716b55b746688c75ca063043c3a88
ArdaEfe <ardaefesilek35@gmail.com>
2025-09-20 11:51:03 +0300

    Google Analytics kodu ve TypeScript fix eklendi

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 // app/layout.js
+
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,9 +19,23 @@ export const metadata = {
   description: "Electrical & Electronics Engineering Student",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="tr">
+      <head>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-XXXXXXXXXX');
+            `,
+          }}
+        />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
